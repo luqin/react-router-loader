f7ec33b1f639230ac72cb77fa8f76b8208cdcbc9
luqin <h-io@qq.com>
2015-11-28 16:15:43 +0800

    Check if component is mounted before setting state.
    Prevents "Uncaught Error: Invariant Violation: replaceState(...): Can
    only update a mounted or mounting component." -- which can occur if the
    proxy component is unmounted before download completes.
    https://github.com/liouh/react-proxy-loader/commit/a55dc081ef9e0df51f6e7a9fd226490c93ec8553

--- mixinReactProxy.js
+++ mixinReactProxy.js
@@ -1,33 +1,34 @@
-
 // Shorter version of https://github.com/sindresorhus/object-assign/blob/master/index.js
 var assign = Object.assign || function (target, source) {
     var keys = Object.keys(Object(source));
-    for (var i=0; i<keys.length; i++) {
-        target[keys[i]] = source[keys[i]];
+    for (var i = 0; i < keys.length; i++) {
+      target[keys[i]] = source[keys[i]];
     }
     return target;
-};
+  };
 
-module.exports = function(React, desc) {
-    desc.displayName = "ReactRouterProxy";
-    desc.render = function() {
-        var Component = this.state.component;
-        if(Component) {
-            return React.createElement(Component, assign({ref: "componentProxy"}, this.props), this.props.children);
-        } else if(this.renderUnavailable) {
-            return this.renderUnavailable();
-        } else {
-            return null;
+module.exports = function (React, desc) {
+  desc.displayName = "ReactRouterProxy";
+  desc.render = function () {
+    var Component = this.state.component;
+    if (Component) {
+      return React.createElement(Component, assign({ref: "componentProxy"}, this.props), this.props.children);
+    } else if (this.renderUnavailable) {
+      return this.renderUnavailable();
+    } else {
+      return null;
+    }
+  };
+  desc.getInitialState = function () {
+    return {component: this.loadComponent()};
+  };
+  desc.componentDidMount = function () {
+    if (!this.state.component) {
+      this.loadComponent(function (component) {
+        if (this.isMounted()) {
+          this.setState({component: component});
         }
-    };
-    desc.getInitialState = function() {
-        return { component: this.loadComponent() };
-    };
-    desc.componentDidMount = function() {
-        if(!this.state.component) {
-            this.loadComponent(function(component) {
-                this.setState({ component: component });
-            }.bind(this));
-        }
-    };
+      }.bind(this));
+    }
+  };
 };
