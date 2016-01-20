d905aa63dbf787e74adf9f76ca5055089dd0daa0
luqin <luqin2006@163.com>
2016-01-20 14:15:56 +0800

    fix Warning: setState(...): Can only update a mounted or mounting component.

--- mixinReactProxy.js
+++ mixinReactProxy.js
@@ -1,25 +1,28 @@
 module.exports = function (React, desc) {
   desc.displayName = "ReactRouterProxy";
+  desc.getInitialState = function () {
+    return { component: this.loadComponent() };
+  };
+  desc.componentDidMount = function () {
+    this.___isMounted = true;
+    if (!this.state.component) {
+      this.loadComponent(function (component) {
+        if (this.___isMounted) {
+          this.setState({ component: component });
+        }
+      }.bind(this));
+    }
+  };
+  desc.componentWillUnmount = function () {
+    this.___isMounted = false;
+  };
   desc.render = function () {
     var Component = this.state.component;
     if (Component) {
       return React.createElement(Component, this.props, this.props.children);
     } else if (this.renderUnavailable) {
       return this.renderUnavailable();
-    } else {
-      return null;
     }
-  };
-  desc.getInitialState = function () {
-    return {component: this.loadComponent()};
-  };
-  desc.componentDidMount = function () {
-    if (!this.state.component) {
-      this.loadComponent(function (component) {
-        if (this.isMounted()) {
-          this.setState({component: component});
-        }
-      }.bind(this));
-    }
+    return null;
   };
 };
